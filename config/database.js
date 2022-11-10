015f02843c8659a4798dc34427f6474b794fafd3
tedsemon <83754705+tedsemon@users.noreply.github.com>
2022-11-09 21:47:35 -0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       port: env.int('DATABASE_PORT', 25060),
       database: env('DATABASE_NAME', 'defaultdb'),
       user: env('DATABASE_USERNAME', 'developer'),
-      password: env('DATABASE_PASSWORD', 'AVNS_N6NDS3qDzMpcpsSmqWC'),
+      password: env('DATABASE_PASSWORD', 'AVNS_0ExXHFY6iDvT470Qqdq'),
       ssl: env.bool('DATABASE_SSL', true),
     },
   },
